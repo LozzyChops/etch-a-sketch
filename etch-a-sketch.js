3470849e58d6ef05e3dc232069a470df7dc42497
LozzyChops <lephilim@gmail.com>
2022-12-29 17:40:25 -0600

    Add button
    Button generates a new grid based on user input.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,34 @@
 const gridContainer = document.querySelector('#grid-container');
+const btn = document.getElementById("btn");
+let userChoice = 10;
 
-function makeGrid(number){
-    gridContainer.style.setProperty('--grid-rows', number);
-    gridContainer.style.setProperty('--grid-columns', number);
-    for (i = 0; i < (number * number); i++) {
+for (i = 0; i < (userChoice*userChoice); i++) {
+    debugger;
+        
+    const gridItem = document.createElement("div");
+    gridItem.className = "grid-item";
+                
+    gridContainer.appendChild(gridItem);
+    document.getElementsByClassName("grid-item")[i].addEventListener("mouseenter", () => gridItem.className = "hover");
+    document.getElementsByClassName("grid-item")[i].addEventListener("mouseleave", () => gridItem.className = "after-hover");
+    
+    gridContainer.style.setProperty('--grid-rows', userChoice);
+    gridContainer.style.setProperty('--grid-columns', userChoice);
+};
+    
+
+function makeGrid() {
+    debugger;
+    let oldChoice = userChoice;
+    userChoice = prompt("What size grid would you like?", "example: '10' yields a 10x10 grid");
+
+    for (i = 0; i < (oldChoice*oldChoice); i++) {
+        gridItem = gridContainer.firstElementChild;
+        gridContainer.removeChild(gridItem);
+    }
+    debugger;
+    
+    for (i = 0; i < (userChoice*userChoice); i++) {
         const gridItem = document.createElement("div");
         gridItem.className = "grid-item";
                 
@@ -11,6 +36,9 @@ function makeGrid(number){
         document.getElementsByClassName("grid-item")[i].addEventListener("mouseenter", () => gridItem.className = "hover");
         document.getElementsByClassName("grid-item")[i].addEventListener("mouseleave", () => gridItem.className = "after-hover");
     };
-};
+    debugger;
 
-makeGrid(15);
+    gridContainer.style.setProperty('--grid-rows', userChoice);
+    gridContainer.style.setProperty('--grid-columns', userChoice);
+    debugger;
+};
